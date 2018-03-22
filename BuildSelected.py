6fd58b64339671ab9f411436836a850315f7bc45
Martin Bonde <martin@bonde.dk>
2018-03-22 15:09:45 +0100

    Add files via upload
    Changed behavior to mimic R-studio.
    Use ctrl-enter to send commands from Sublime.
    
    Gekko-Sublime no longer checks Gekko command line parameter for remote control option.
    Remote control option should be set to YES in Gekko (this can be done automatically using the ini file).
    The file open in Sublime is used as working drive unless a different folder was supplied with a command line parameter when Gekko was opened.

--- BuildSelected.py
+++ BuildSelected.py
@@ -1,8 +1,11 @@
 import sublime_plugin
+import sublime
 import subprocess as sp
 import os
+import shutil
 import time
 import re
+
 
 class BuildSelectedCommand(sublime_plugin.TextCommand):
     """
@@ -10,7 +13,6 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
     If no Gekko process is found, open Gekko with remote interface enabled and working directory set to current file directory.
     """
     def run(self, edit):
-
         file_path = self.view.window().active_view().file_name()
         extenstion = os.path.splitext(file_path)[1]
 
@@ -21,18 +23,24 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
             proc = sp.Popen(cmd, shell=True, stdout=sp.PIPE, universal_newlines=True)
             for line in proc.stdout:
                 if "Gekko.exe" in line:
-                    if "-folder" in line and "option interface remote = yes;" in line:
+                    if "folder" in line:
                         p = re.compile("Gekko.exe.+[-]folder[:](\S+)")
                         working_folder = p.search(line).group(1)
-                        print(working_folder)
-                        # working_folder = parse("Gekko.exe{}-folder:{} {}", line)[1]  # parse module not included in Sublime
-                        break
+                    else:
+                        working_folder = os.path.dirname(file_path)
+                    print("Running Gekko instance found. Remote control commands are written to %s" % working_folder)
+                    break
             else:
-                working_folder = os.path.dirname(file_path)
-                start_code = "option interface remote = yes;"
-                cmd = [os.environ["gekko"], "-folder:"+working_folder, start_code]
-                sp.Popen(cmd, stdin=sp.PIPE, universal_newlines=True)
-                time.sleep(5)
+                if shutil.which("gekko"):
+                    working_folder = os.path.dirname(file_path)
+                    start_code = "option interface remote = yes;"
+                    cmd = ["gekko", "-folder:"+working_folder, start_code]
+                    print("New Gekko instance opened. Remote control commands are written to %s" % working_folder)
+                    sp.Popen(cmd, stdin=sp.PIPE, universal_newlines=True)
+                    time.sleep(5)
+                else:
+                    sublime.error_message("No open Gekko instance was found. Please open Gekko (or set a path to Gekko so it can be opened automatically).")
+                    exit()
 
             # Remove quotes from working folder path
             if working_folder.startswith('"') and working_folder.endswith('"'):
@@ -42,10 +50,13 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
             selected = ""
             for region in self.view.sel():
                 selected += self.view.substr(region)
-            # If none is selected, select the currect line
+            # If none is selected, get the current line and move cursor to next line
             if not selected:
                 line = self.view.line(region)
                 selected = self.view.substr(line)
+                print(line)
+                self.view.sel().clear()
+                self.view.sel().add(line.b+1)
 
             # The code is run by writing to the remote.gcm file in the working folder
             remote = os.path.join(working_folder, "remote.gcm")
@@ -56,7 +67,11 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
             # After allowing enough time for gekko to update, delete the remote file
             # time.sleep(0.3)
             # os.remove(remote)
+
+            # Center view on selection if out of view
+            self.view.show(self.view.line(region))
+
         else:
-            print("Selected build only works with .gcm files")
+            sublime.error_message("Selected build currently only works with .gcm files")
 
 
