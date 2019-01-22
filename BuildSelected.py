a989a5b2041591b44b5edc893ec3055e668660ac
Martin Kirk Bonde (DREAM) <B031441@prod.sitad.dk>
2019-01-22 11:27:26 +0100

    New sublime-syntax file

--- BuildSelected.py
+++ BuildSelected.py
@@ -12,9 +12,9 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
     Run selected Gekko commands by sending them to the remote file in the working directory.
     If no Gekko process is found, open Gekko with remote interface enabled and working directory set to current file directory.
     """
+
     def run(self, edit):
         file_path = self.view.window().active_view().file_name()
-        extension = os.path.splitext(file_path)[1]
 
         settings = sublime.load_settings("Gekko.sublime-settings")
         gekko_path = os.path.expandvars(settings.get("gekko_path")) + "\\Gekko.exe"
@@ -26,21 +26,21 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
         # If Gekko is open, get the working folder
         cmd = 'WMIC PROCESS get Caption,Commandline,Processid'
         with sp.Popen(cmd, shell=True, stdout=sp.PIPE, universal_newlines=True) as proc:
-          for line in proc.stdout:
-              if "Gekko.exe" in line:
-                  print("Running Gekko instance found. Remote control commands are written to %s" % remote_file_path)
-                  break
-          # Else open Gekko with the current dir as working folder and interface remote enabled
-          else:
-              if os.path.isfile(gekko_path):
-                  working_folder = os.path.dirname(file_path)
-                  start_code = "option interface remote = yes; OPTION interface remote file = '%s';" % remote_file_path
-                  cmd = [gekko_path, "-folder:"+working_folder, start_code]
-                  print("New Gekko instance opened. Remote control commands are written to %s" % remote_file_path)
-                  sp.Popen(cmd, stdin=sp.PIPE, universal_newlines=True)
-                  time.sleep(5)
-              else:
-                  return sublime.error_message("Open Gekko or add a path to Gekko in Package Settings.")
+            for line in proc.stdout:
+                if "Gekko.exe" in line:
+                    print("Running Gekko instance found. Remote control commands are written to %s" % remote_file_path)
+                    break
+        # Else open Gekko with the current dir as working folder and interface remote enabled
+            else:
+                if os.path.isfile(gekko_path):
+                    working_folder = os.path.dirname(file_path)
+                    start_code = "option interface remote = yes; OPTION interface remote file = '%s';" % remote_file_path
+                    cmd = [gekko_path, "-folder:" + working_folder, start_code]
+                    print("New Gekko instance opened. Remote control commands are written to %s" % remote_file_path)
+                    sp.Popen(cmd, stdin=sp.PIPE, universal_newlines=True)
+                    time.sleep(5)
+                else:
+                    return sublime.error_message("Open Gekko or add a path to Gekko in Package Settings.")
 
         # Get selected text
         selected = ""
@@ -55,7 +55,7 @@ class BuildSelectedCommand(sublime_plugin.TextCommand):
                 self.view.insert(edit, line.b, ";\n")
                 line = self.view.line(region)
             self.view.sel().clear()
-            self.view.sel().add(line.b+1)
+            self.view.sel().add(line.b + 1)
 
         # The code is run by writing to the remote.gcm file
         print("Sending '{}' to gekko".format(selected))
